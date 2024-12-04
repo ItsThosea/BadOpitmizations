3b0380726a40e214fe255adb53e51cae60dfb2d8
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-12-03 23:45:25 -0500

    fix: compatibility with 1.21.4

--- common/src/main/java/me/thosea/badoptimizations/mixin/MixinParticleManager.java
+++ common/src/main/java/me/thosea/badoptimizations/mixin/MixinParticleManager.java
@@ -3,8 +3,6 @@ package me.thosea.badoptimizations.mixin;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.particle.ParticleTextureSheet;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,8 +17,10 @@ import java.util.Queue;
 public class MixinParticleManager {
 	@Shadow @Final private Map<ParticleTextureSheet, Queue<Particle>> particles;
 
-	@Inject(method = "renderParticles", at = @At("HEAD"), cancellable = true)
-	private void onRender(LightmapTextureManager lightmapTextureManager, Camera camera, float tickDelta, CallbackInfo ci) {
+	// implicit, 1.21.4 adds another method with a similar signature
+	// we can target both with this while maintaining 1.21.3 compatibility
+	@Inject(method = "renderParticles*", at = @At("HEAD"), cancellable = true)
+	private void onRender(CallbackInfo ci) {
 		if(particles.isEmpty()) {
 			ci.cancel();
 		}
