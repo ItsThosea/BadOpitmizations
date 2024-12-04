2d6f670c5195c9ca3be85f800204dad150981b78
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-12-03 23:59:33 -0500

    fix: crash on forge/neo servers instead of a proper error message

--- forge/src/main/java/me/thosea/badoptimizations/forge/BadOptimizations.java
+++ forge/src/main/java/me/thosea/badoptimizations/forge/BadOptimizations.java
@@ -1,6 +1,8 @@
 package me.thosea.badoptimizations.forge;
 
 import me.thosea.badoptimizations.other.BOConfigScreen;
+import me.thosea.badoptimizations.other.Config;
+import me.thosea.badoptimizations.other.PlatformMethods;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.ConfigScreenHandler;
@@ -9,9 +11,21 @@ import net.neoforged.neoforge.client.ConfigScreenHandler.ConfigScreenFactory;
 @Mod("badoptimizations")
 public class BadOptimizations {
 	public BadOptimizations() {
-		ModLoadingContext.get().registerExtensionPoint(
-				ConfigScreenHandler.ConfigScreenFactory.class,
-				() -> new ConfigScreenFactory((mc, parent) -> new BOConfigScreen(parent))
-		);
+		if(PlatformMethods.isOnServer()) {
+			Config.LOGGER.error("BadOptimizations is a client-only mod, this will have no effect on a server.");
+			return;
+		}
+
+		// avoid loading client classes on server
+		ClientInit.init();
+	}
+
+	private static class ClientInit {
+		private static void init() {
+			ModLoadingContext.get().registerExtensionPoint(
+					ConfigScreenHandler.ConfigScreenFactory.class,
+					() -> new ConfigScreenFactory((mc, parent) -> new BOConfigScreen(parent))
+			);
+		}
 	}
 }
